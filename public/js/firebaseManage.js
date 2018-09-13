68bc53f62b2c452c915ba78e17b05bfa8b02fae0
DanielNempeque <daniel.nempeque@gmail.com>
2018-09-13 10:43:04 -0500

    Ordenacion completada

--- public/js/firebaseManage.js
+++ public/js/firebaseManage.js
@@ -5,8 +5,11 @@ $(document).ready(function () {
     var j = 1;
     var k = 1;
 
+    var equiposUsab;
+    var equiposCol;
     dbref.on('value', snap =>{
-        $("#table_body tr").remove();
+        equiposUsab = [];
+        equiposCol = [];
         while((snap.child("team_"+i).val() != null)){
             var numEquipo = snap.child("team_"+i).child("numEquipo").val();
             var nomEquipo = snap.child("team_"+i).child("nomEquipo").val();
@@ -18,16 +21,83 @@ $(document).ready(function () {
             var puntajeDecoracion = snap.child("team_"+i).child("puntajeDecoracion").val();
             var total = snap.child("team_"+i).child("total").val();
             var estado= snap.child("team_"+i).child("estado").val();
-            $('#'+i+'-1').text(numEquipo);
-            $('#'+i+'-2').text(nomEquipo);
-            $('#'+i+'-3').text(tiempoTotal);
+            if(institucion == "usabana"){
+                equiposUsab.push({
+                    numEquipo: numEquipo,
+                    nomEquipo: nomEquipo,
+                    institucion: institucion,
+                    tiempoTotal: tiempoTotal,
+                    puntajeDiseño: puntajeDiseño,
+                    puntajeConstructor: puntajeConstructor,
+                    puntajeTiempo: puntajeTiempo,
+                    puntajeDecoracion: puntajeDecoracion,
+                    total: total,
+                    estado: estado
+                });
+
+
+            }else{
+                equiposCol.push({
+                    numEquipo: numEquipo,
+                    nomEquipo: nomEquipo,
+                    institucion: institucion,
+                    tiempoTotal: tiempoTotal,
+                    puntajeDiseño: puntajeDiseño,
+                    puntajeConstructor: puntajeConstructor,
+                    puntajeTiempo: puntajeTiempo,
+                    puntajeDecoracion: puntajeDecoracion,
+                    total: total,
+                    estado: estado
+                });
+            }
+
             $("#table_body").append("<tr><td>"+numEquipo+"</td><td>"+nomEquipo+"</td><td>"+tiempoTotal+"</td></tr>");
             i++;
             k=i;
         }
+        equiposCol.sort(function(a,b){
+            if(a.tiempoTotal>b.tiempoTotal){
+                return 1;
+            }
+            if(a.tiempoTotal<b.tiempoTotal){
+               return -1;
+            }
+            return 0;
+        });
+        equiposUsab.sort(function(a,b){
+            if(a.tiempoTotal>b.tiempoTotal){
+                return 1;
+            }
+            if(a.tiempoTotal<b.tiempoTotal){
+               return -1;
+            }
+            return 0;
+        });
+        buildDataCol();
+        buildDataUsab();
         i = 1;
     });
+    function buildDataCol(){
+        console.log(equiposCol);
+        var i = 1;
+        equiposCol.forEach(function(x){
+            $('#'+i+'-4').text(x.numEquipo);
+            $('#'+i+'-5').text(x.nomEquipo);
+            $('#'+i+'-6').text(x.tiempoTotal);
+            i++;
+        });
+    }
 
+    function buildDataUsab(){
+        console.log(equiposUsab);
+        var i = 1;
+        equiposUsab.forEach(function(x){
+            $('#'+i+'-1').text(x.numEquipo);
+            $('#'+i+'-2').text(x.nomEquipo);
+            $('#'+i+'-3').text(x.tiempoTotal);
+            i++;
+        });
+    }
     $('#boton_add').click(function () {
         if(confirm("¿Esta seguro que quiere agregar?")){
             agregarData();
